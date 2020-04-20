22ffb1f161a990bd170c8c9d16f96ec0ea38ec3e
Roman Tsvetkov <tsvetkovv22@gmail.com>
2020-04-20 21:28:38 +0300

    feat: implemented the selector component

---
+++ src/app/shared/date.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import * as moment from 'moment';
+import { BehaviorSubject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class DateService {
+  public date: BehaviorSubject<moment.Moment> = new BehaviorSubject(moment());
+
+  changeMonth(direction: number) {
+    const value = this.date.value.add(direction, 'month');
+    this.date.next(value);
+  }
+}

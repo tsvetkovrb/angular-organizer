cf0a5e4d02e9e82c9943686a7c40447d8cb8f1f2
Roman Tsvetkov <tsvetkovv22@gmail.com>
2020-04-21 19:39:11 +0300

    feat: implemented the calendar component

--- src/app/shared/date.service.ts
+++ src/app/shared/date.service.ts
@@ -12,4 +12,13 @@ export class DateService {
     const value = this.date.value.add(direction, 'month');
     this.date.next(value);
   }
+
+  changeDate(date: moment.Moment) {
+    const value = this.date.value.set({
+      date: date.date(),
+      month: date.month(),
+    });
+
+    this.date.next(value);
+  }
 }

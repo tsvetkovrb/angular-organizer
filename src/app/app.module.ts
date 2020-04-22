6e3deb62c0e432fc9c21d2db6d6f342903c4ccd3
Roman Tsvetkov <tsvetkovv22@gmail.com>
2020-04-22 19:06:02 +0300

    feat: implemented the organizer component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { CalendarComponent } from './calendar/calendar.component';
 import { SelectorComponent } from './selector/selector.component';
 import { OrganizerComponent } from './organizer/organizer.component';
 import { MomentPipe } from './shared/moment.pipe';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -15,10 +17,8 @@ import { MomentPipe } from './shared/moment.pipe';
     OrganizerComponent,
     MomentPipe,
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule, FormsModule, ReactiveFormsModule, HttpClientModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/organizer/organizer.component.ts
+++ src/app/organizer/organizer.component.ts
@@ -1,15 +1,55 @@
 import { Component, OnInit } from '@angular/core';
+import { DateService } from '../shared/date.service';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { TasksService, Task } from '../shared/tasks.servise';
+import { switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-organizer',
   templateUrl: './organizer.component.html',
-  styleUrls: ['./organizer.component.scss']
+  styleUrls: ['./organizer.component.scss'],
 })
 export class OrganizerComponent implements OnInit {
+  form: FormGroup;
+  tasks: Task[] = [];
 
-  constructor() { }
+  constructor(
+    public dateService: DateService,
+    private tasksService: TasksService
+  ) {}
 
   ngOnInit(): void {
+    this.dateService.date
+      .pipe(switchMap((value) => this.tasksService.load(value)))
+      .subscribe((tasks) => {
+        this.tasks = tasks;
+      });
+
+    this.form = new FormGroup({
+      title: new FormControl('', Validators.required),
+    });
   }
 
+  submit() {
+    const { title } = this.form.value;
+    const task: Task = {
+      title,
+      date: this.dateService.date.value.format('DD-MM-YYYY'),
+    };
+
+    this.tasksService.create(task).subscribe(
+      (responseTask) => {
+        this.tasks.push(responseTask);
+        this.form.reset();
+      },
+      (error) => console.error(error)
+    );
+  }
+
+  remove(task: Task) {
+    this.tasksService.remove(task).subscribe(
+      () => this.tasks = this.tasks.filter((t) => t.id !== task.id),
+      (error) => console.log(error)
+    );
+  }
 }
